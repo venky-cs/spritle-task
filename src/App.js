954980dd69e7b15396ee333e8bb18f613e95216b
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-07-13 13:55:20 +0530

    create-google-auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebaseConfig'
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+  IfFirebaseAuthed,
+  IfFirebaseAuthedAnd,
+} from "@react-firebase/auth";
+require("dotenv").config();
+
+
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
+      <div>
+        <button
+          onClick={() => {
+            const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(googleAuthProvider);
+          }}
+        >
+          Sign In with Google
+        </button>
+
+        <button
+          onClick={() => {
+            firebase.auth().signOut();
+          }}
+        >
+          Sign Out
+        </button>
+        
+        <FirebaseAuthConsumer>
+          {({ isSignedIn, user, providerId }) => {
+            return (
+              // <pre style={{ height: 300, overflow: "auto" }}>
+              //   {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
+              // </pre>
+              null
+            );
+          }}
+        </FirebaseAuthConsumer>
+        <div>
+          <IfFirebaseAuthed>
+            {() => {
+              return <div>You are authenticated</div>;
+            }}
+          </IfFirebaseAuthed>
+          <IfFirebaseAuthedAnd
+            filter={({ providerId }) => providerId !== "anonymous"}
+          >
+            {({ providerId }) => {
+              return <div>You are authenticated with {providerId}</div>;
+            }}
+          </IfFirebaseAuthedAnd>
+        </div>
+      </div>
+    </FirebaseAuthProvider>
   );
 }
 
